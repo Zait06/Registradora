d9cf3e2e2e87da5746edf4c53ab30ebd470d69c7
Zait Hernandez <zait-hdez@hotmail.com>
2022-06-20 18:52:15 -0500

    Save in localstorage

--- dist/js/burger.js
+++ dist/js/burger.js
@@ -1,12 +1,14 @@
 var ok_btn = document.getElementById('ok')
 var cancel_btn = document.getElementById('cancel')
 
-ok_btn.addEventListener('click', () => {
-    window.location.href = '../index.html'
+ok_btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const order = localStorage.getItem('order');
+    alert(order)
+    // window.location.href = '../index.html'
 })
 
 
 cancel_btn.addEventListener('click', () => {
-    alert('cancel')
     window.location.href = '../index.html'
 })
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,18 @@
 var btnPizza = document.getElementById("burger_btn")
 
+window.onload = () => {
+    const date = new Date()
+    const newOrder = `{id: '${date.getTime()}', end: false, order:[]}`
+    let order = localStorage.getItem('order');
+    if (!order) {
+        localStorage.setItem('order', newOrder);
+        order = newOrder;
+    }
+
+    console.log(order);
+
+}
+
 btnPizza.addEventListener('click', () => {
     window.location.href = 'pages/burger.html'
 })
