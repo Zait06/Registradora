5541ebe55118af61ee5158310839f97b7dd8596c
Zait06 <zait-hdez@hotmail.com>
2022-07-09 21:19:56 -0500

    Task #2: Order timestamp doesn't refresh

--- dist/js/Order.js
+++ dist/js/Order.js
@@ -1,68 +1,83 @@
-export const invoke = window.__TAURI__.invoke
+export const invoke = window.__TAURI__.invoke;
+
+const fifteenMinutes = 15 * 60 * 1000;
 
 export class Order {
-    constructor() {
-        if (localStorage.getItem('order'))
-            this.initFromStorage();
-        else
-            this.init();
-    }
+  constructor() {
+    if (localStorage.getItem('order')) this.initFromStorage();
+    else this.init();
+  }
 
-    init(){
-        this.timestamp = new Date().getTime();
-        this.order = [];
-        this.end = false;
-        localStorage.setItem('order', this.toString())
-    }
+  init() {
+    this.timestamp = new Date().getTime();
+    this.order = {};
+    this.end = false;
+    localStorage.setItem('order', this.toString());
+  }
 
-    isFinishOrder() {
-        const orderStr = localStorage.getItem('order');
-        const order = JSON.parse(orderStr);
-        return order.end;
-    }
+  limitTime() {
+    const orderData = JSON.parse(localStorage.getItem('order'));
+    return (
+      orderData.timestamp - new Date().getTime() > fifteenMinutes
+    );
+  }
 
-    initFromStorage() {
-        if (this.isFinishOrder() && this.isFinishOrder() === undefined) {
-            localStorage.clear();
-            localStorage.setItem('order', createOrder());
-        } else {
-            this.fromLocalStorage();
-        }
-    }    
+  isFinishOrder() {
+    const orderStr = localStorage.getItem('order');
+    const order = JSON.parse(orderStr);
+    return order.end;
+  }
 
-    toObject() {
-        return {
-            timestamp: this.timestamp,
-            order: this.order,
-            end: this.end
-        }
+  initFromStorage() {
+    if (this.isFinishOrder() && this.limitTime()) {
+      localStorage.clear();
+      localStorage.setItem('order', createOrder());
+    } else {
+      this.fromLocalStorage();
     }
+  }
 
-    toString() {
-        return JSON.stringify(this.toObject());
-    }
+  toObject() {
+    return {
+      timestamp: this.timestamp,
+      order: this.order,
+      end: this.end,
+    };
+  }
 
-    toJSON() {
-        return JSON.parse(this.toString());
-    }
+  toString() {
+    return JSON.stringify(this.toObject());
+  }
 
-    fromLocalStorage() {
-        const orderData = JSON.parse(localStorage.getItem('order'));
-        this.timestamp = orderData.timestamp;
-        this.order = orderData.order;
-        this.end = orderData.end
-    }
+  toJSON() {
+    return JSON.parse(this.toString());
+  }
 
-    createOrder() {
-        this.timestamp = new Date().getTime();
-        return this.toString();
-    }
+  fromLocalStorage() {
+    const orderData = JSON.parse(localStorage.getItem('order'));
+    this.timestamp = orderData.timestamp;
+    this.order = orderData.order;
+    this.end = orderData.end;
+  }
 
-    saveInDatabase() {
-        this.end = true;
-        const order = this.toString()
-        localStorage.setItem('order', order);
-        invoke('save_order', { order });
-        this.init()
-    }
+  createOrder() {
+    this.timestamp = new Date().getTime();
+    return this.toString();
+  }
+
+  saveInDatabase() {
+    this.end = true;
+    const order = this.toString();
+    localStorage.setItem('order', order);
+    this.init();
+    invoke('save_order', { order });
+  }
+
+  add(data) {
+    this.order.push(data);
+  }
+
+  remove(key, idx) {
+    this.order[key].splice(idx, 1);
+  }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,21 @@
-use serde_json::{Value};
+use serde_json::Value;
 use std::fs::File;
 use std::io::prelude::*;
 
+mod dbcontroller;
+
 #[tauri::command]
 fn save_order(order: String) {
-  let json_order: Value = serde_json::from_str(&order).unwrap();
-  println!("Time: {}", json_order["timestamp"]);
-
-  let mut f = File::create("order.txt").unwrap();
-  f.write_all(order.as_bytes()).unwrap();
+    let json_order: Value = serde_json::from_str(&order).unwrap();
+    println!("Time: {}", json_order["timestamp"]);
+    dbcontroller::print_hello();
+    let mut f = File::create("order.txt").unwrap();
+    f.write_all(order.as_bytes()).unwrap();
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![save_order])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![save_order])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
